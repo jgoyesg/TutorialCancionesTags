c8c09a7e4e04d16dc02ac4be71c83ac0fff901fe
jgoyes <jgoyes@elthon.org>
2023-09-23 23:06:36 -0500

    Release

--- tests/test_interprete.py
+++ tests/test_interprete.py
@@ -18,6 +18,7 @@ class InterpreteTestCase(unittest.TestCase):
     print('dessde lara mas')
     print('dessde lara mas')
     print('dessde lara mas')
+    print('dessde lara mas')
     def setUp(self):
         self.session = Session()
         self.coleccion = Coleccion()
